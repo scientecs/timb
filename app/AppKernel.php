34e62f253c44f8a4eb732115183de03eb86ee3e5
scientecs <grishatkin2013@mail.ru>
2016-02-20 20:57:22 +0200

    feat(master) "add base template and add site, admin bundles"

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new SiteBundle\SiteBundle(),
+            new AdminBundle\AdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
